1fcbb24444795355c87e3f1fe142459a3c493c52
THXBurke <rachelwheels@gmail.com>
2016-05-03 14:54:13 -0700

    email

--- email.js
+++ email.js
@@ -1,14 +1,19 @@
 const email = require(__dirname + '/lib/chron_email.js');
+const Plan = require(__dirname + '/models/plan.js');
+const errorHandler = require(__dirname + '/lib/errorHandler.js');
 
-    var now = new Date();
+var now = new Date().getTime();
+Plan.find( { reminderDate: { $lt: now } }, (err, planData) => {
+  if (err) return errorHandler(err);
     const mailConfig = {
       from: 'info@preparedfordisaster.org',
       to: 'greg.magdsick@gmail.com',
       subject: 'Your Emergency Disaster Plan as of: ' + now,
       text: '',
-      html: 'msg: message sent'
+      html: JSON.stringify(planData)
     };
     email.sendMail(mailConfig, (err, info) => {
       if (err) return console.log(err);
       console.log('Message sent: ' + info.response);
+    });
 });

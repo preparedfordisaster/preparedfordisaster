911eadb5315402bd36f28bfee00ac7575a12ce32
Katherine Beame <katherine.beame@gmail.com>
2016-05-03 12:27:57 -0700

    trying to email

--- email.js
+++ email.js
@@ -1,6 +1,25 @@
 #!/usr/bin/env node
+const errorHandler = require(__dirname + '/lib/errorHandler');
+const Plan = require(__dirname + '/models/plan');
+const email = require(__dirname + '/lib/chron_email.js');
 
 function sayHello() {
   console.log('Hello');
 }
 sayHello();
+
+// Plan.findOne({ memberID: req.user._id }, (err, planData) => {
+//   if (err) return errorHandler(err);
+  var now = new Date();
+  const mailConfig = {
+    from: 'info.preparedfordisaster@gmail.com',
+    to: 'greg.magdsick@gmail.com',
+    subject: 'Your Emergency Disaster Plan as of: ' + now,
+    text: '',
+    html: 'scheduled stuff'
+  };
+  email.sendMail(mailConfig, (err, info) => {
+    if (err) return console.log(err);
+    console.log('Message sent: ' + info.response);
+  });
+// });

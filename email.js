5502f5f3ad1c62684a8f20efa85cd23bf3d4120f
Katherine Beame <katherine.beame@gmail.com>
2016-05-03 15:45:40 -0700

    trying to use a function

--- email.js
+++ email.js
@@ -3,13 +3,12 @@ const Plan = require(__dirname + '/models/plan.js');
 const errorHandler = require(__dirname + '/lib/errorHandler.js');
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/plan_db');
-
-var now = new Date();
-console.log('now ' + now);
-Plan.find( { reminderFrequency: { $lt: 99 } }, (err, planData) => {
-  console.log('plandata ' + planData);
-  if (err) return errorHandler(err);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/plan_db', function (err, done) => {
+  var now = new Date();
+  console.log('now ' + now);
+  Plan.find( { reminderFrequency: { $lt: 99 } }, (err, planData) => {
+    console.log('plandata ' + planData);
+    if (err) return errorHandler(err);
     const mailConfig = {
       from: 'info@preparedfordisaster.org',
       to: 'greg.magdsick@gmail.com',
@@ -20,6 +19,7 @@ Plan.find( { reminderFrequency: { $lt: 99 } }, (err, planData) => {
     email.sendMail(mailConfig, (err, info) => {
       if (err) return console.log(err);
       console.log('Message sent: ' + info.response);
-      mongoose.disconnect();
+      mongoose.disconnect(done);
     });
+  });
 });

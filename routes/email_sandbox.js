8b8879b0812b37cce20912e96f2936b81803a35b
Phil Nguyen <philnguyen@Phils-MacBook-Pro.local>
2016-05-03 15:55:55 -0700

    mail template, handlebars

--- routes/email_sandbox.js
+++ routes/email_sandbox.js
@@ -3,24 +3,36 @@ const errorHandler = require(__dirname + '/../lib/errorHandler');
 const jwtAuth = require(__dirname + '/../lib/jwt_auth');
 const Plan = require(__dirname + '/../models/plan');
 const email = require(__dirname + '/../lib/chron_email.js');
+const handlebars = require('handlebars');
+const fs = require('fs');
 
 const emailRouter = module.exports = exports = new Router();
 
 
 emailRouter.post('/email', jwtAuth, (req, res) => {
-  Plan.findOne({ memberID: req.user._id }, (err, planData) => {
+   Plan.findOne({ memberID: req.user._id }, (err, planData) => {
     if (err) return errorHandler(err);
+    var template;
+    var body;
+    fs.readFile(__dirname + '/../view/mail_template.html', (err, source) => {
+      if (err) throw err;
+      template = handlebars.compile(source.toString());
+      console.log(source.toString());
+      body = template(planData);
+      console.log('body', body);
+
+    });
     var now = new Date();
     const mailConfig = {
       from: 'info.preparedfordisaster@gmail.com',
       to: 'greg.magdsick@gmail.com',
-      subject: 'Your Emergency Disaster Plan as of: ' + now,
-      text: '',
-      html: JSON.stringify(planData)
+      subject: '[TEST]Your Emergency Disaster Plan as of: ' + now,
+      html: body
     };
     email.sendMail(mailConfig, (err, info) => {
       if (err) return console.log(err);
       console.log('Message sent: ' + info.response);
     });
   });
+  res.status(200).json({ msg: 'Successfully sent!' });
 });

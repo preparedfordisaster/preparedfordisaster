47f6958f9ee03946a63b83b30a7dba4f0a1e717e
Greg Magdsick <greg.magdsick@gmail.com>
2016-05-03 11:12:42 -0700

    email from DB

--- routes/email_sandbox.js
+++ routes/email_sandbox.js
@@ -1,20 +1,25 @@
 const Router = require('express').Router;
-const bodyParser = require('body-parser').json();
+const errorHandler = require(__dirname + '/../lib/errorHandler');
+const Plan = require(__dirname + '/../models/plan');
 const email = require(__dirname + '/../lib/chron_email.js');
 
 const emailRouter = module.exports = exports = new Router();
 
-const mailConfig = {
-    from: 'info.preparedfordisaster@gmail.com',
-    to: 'greg.magdsick@gmail.com',
-    subject: 'Your Emergency Disaster Plan',
-    text: '',
-    html: 'testing your email'
-  };
 
-emailRouter.post('/email', bodyParser, () => {
-  email.sendMail(mailConfig, (err, info) => {
-    if (err) return console.log(err);
-    console.log('Message sent: ' + info.response);
+emailRouter.post('/email', () => {
+  Plan.findOne({ memberID: '5728e4b1443b8efd555629ea' }, (err, planData) => {
+    if (err) return errorHandler(err);
+    var now = new Date();
+    const mailConfig = {
+      from: 'info.preparedfordisaster@gmail.com',
+      to: 'greg.magdsick@gmail.com',
+      subject: 'Your Emergency Disaster Plan as of: ' + now,
+      text: '',
+      html: JSON.stringify(planData)
+    };
+    email.sendMail(mailConfig, (err, info) => {
+      if (err) return console.log(err);
+      console.log('Message sent: ' + info.response);
+    });
   });
 });

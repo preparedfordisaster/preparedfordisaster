217deb2be90f47821e68c2eec974b7015ce3d2c7
Phil Nguyen <philnguyen@Phils-MacBook-Pro.local>
2016-05-03 22:44:24 -0700

    added readFile promise module to account for async issues with email template

--- routes/email_sandbox.js
+++ routes/email_sandbox.js
@@ -4,34 +4,32 @@ const jwtAuth = require(__dirname + '/../lib/jwt_auth');
 const Plan = require(__dirname + '/../models/plan');
 const email = require(__dirname + '/../lib/chron_email.js');
 const handlebars = require('handlebars');
-const fs = require('fs');
+const readFile = require('fs-readfile-promise');
 
 const emailRouter = module.exports = exports = new Router();
 
 
 emailRouter.post('/email', jwtAuth, (req, res) => {
-   Plan.findOne({ memberID: req.user._id }, (err, planData) => {
+  Plan.findOne({ memberID: req.user._id }, (err, planData) => {
     if (err) return errorHandler(err);
     var template;
     var body;
-    fs.readFile(__dirname + '/../view/mail_template.html', (err, source) => {
+    readFile(__dirname + '/../view/mail_template.html').then((buffer) => {
       if (err) throw err;
-      template = handlebars.compile(source.toString());
-      console.log(source.toString());
+      template = handlebars.compile(buffer.toString());
       body = template(planData);
       console.log('body', body);
-
-    });
-    var now = new Date();
-    const mailConfig = {
-      from: 'info.preparedfordisaster@gmail.com',
-      to: 'greg.magdsick@gmail.com',
-      subject: '[TEST]Your Emergency Disaster Plan as of: ' + now,
-      html: body
-    };
-    email.sendMail(mailConfig, (err, info) => {
-      if (err) return console.log(err);
-      console.log('Message sent: ' + info.response);
+      var now = new Date();
+      const mailConfig = {
+        from: 'info.preparedfordisaster@gmail.com',
+        to: 'phillip.d.nguyen23@gmail.com',
+        subject: '[TEST]Your Emergency Disaster Plan as of: ' + now,
+        html: body
+      };
+      email.sendMail(mailConfig, (err, info) => {
+        if (err) return console.log(err);
+        console.log('Message sent: ' + info.response);
+      });
     });
   });
   res.status(200).json({ msg: 'Successfully sent!' });
